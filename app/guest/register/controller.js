757f6a1a3d35ac8b93c4b36ec3dc7f60de29182f
Christopher Duke <cd.duke@gmail.com>
2016-08-17 11:53:28 -0500

    register styling

--- app/guest/register/controller.js
+++ app/guest/register/controller.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    registerUser(formValues) {
+      const newUser = this.store.createRecord('user', formValues);
+
+      newUser.save().then(() => {
+        this.transitionToRoute('login');
+      });
+    },
+  },
 });
