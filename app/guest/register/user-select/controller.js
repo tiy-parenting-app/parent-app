2c22f25ae3dec23d311821ced5341afbdfd0d91f
Christopher Duke <cd.duke@gmail.com>
2016-08-22 13:06:50 -0500

    attepmted user-select controller

---
+++ app/guest/register/user-select/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    userSelect(formValues) {
+    const newSelect = this.store.createRecord('user', formValues);
+
+    newSelect.save().then(() => {
+      this.transitionToRoute('guest');
+    });
+  },
+
+ },
+});
