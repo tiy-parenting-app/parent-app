e04c5a7f4cd7a02c5352ad821b49fc52724acd30
Alex T Leach <alextleach@gmail.com>
2016-08-23 10:11:04 -0500

    users can now log in

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import config from 'parent-app/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-
+  // authorizer: 'authorizer:token',
   namespace: config.DS.namespace,
   host: config.DS.host,
 
