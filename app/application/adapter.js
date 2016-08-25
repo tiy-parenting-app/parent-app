d41654b1f2efe80a8907e8d009ff7f2958e9c8ba
Christopher Duke <cd.duke@gmail.com>
2016-08-25 13:58:07 -0500

    DataAdapterMixin added

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 import config from 'parent-app/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
-  // authorizer: 'authorizer:token',
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:token',
   namespace: config.DS.namespace,
   host: config.DS.host,
 
