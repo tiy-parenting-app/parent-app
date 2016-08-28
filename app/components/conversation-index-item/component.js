b5f2a6617fee0b678bd7f1861bfa6c38fbb9f130
Christopher Duke <cd.duke@gmail.com>
2016-08-27 20:13:47 -0500

    participant data displays in index

--- app/components/conversation-index-item/component.js
+++ app/components/conversation-index-item/component.js
@@ -8,7 +8,6 @@ export default Ember.Component.extend({
     this._super(...arguments);
     const currentUser = this.get('session.currentUser');
 
-    // Find the other user out of conversation.participants
     this.get('conversation.participants').then((participants)=> {
       participants.forEach((participant) => {
         if (participant.get('user.id') !== currentUser.get('id')) {
