302909fb50157c7f9d016e0a699355b51353f5bb
Alex T Leach <alextleach@gmail.com>
2016-08-28 20:02:41 -0500

    unsure why the updateProfile action doesnt work. maybe it needs some backend work

--- app/profile/edit/controller.js
+++ app/profile/edit/controller.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    updateProfile(user, formValues){
-      user.get('profile').then((profile) => {
-        profile.setProperties({...formValues});
-        profile.save().then(() => {
-          this.get('flashMessages').success('Your profile has been updated!');
-        });
-      });
-    },
+  updateProfile(newEdits, formValues) {
+    newEdits.setProperties(formValues);
+
+    newEdits.save().then(() => {
+      this.get('flashMessages').success('Profile updated.');
+      this.transitionToRoute('profile.view.me');
+    });
   },
+},
 });
