40c7733acf58f91bfde4e52a3e1c7e27ecbd7b48
Alex T Leach <alextleach@gmail.com>
2016-08-29 11:52:36 -0500

    heroku changes

--- app/profile/edit/controller.js
+++ app/profile/edit/controller.js
@@ -3,13 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
   updateProfile(newEdits, formValues) {
-    newEdits.setProperties(formValues);
-    debugger;
+    newEdits.then((profile) => {
+      profile.setProperties(formValues);
 
-    newEdits.save().then(() => {
-      this.get('flashMessages').success('Profile updated.');
-      this.transitionToRoute('profile.view.me');
-    });
+      profile.save().then(() => {
+        this.get('flashMessages').success('Profile updated.');
+        this.transitionToRoute('profile.view.me');
+      });
+    })
   },
 },
 });
--- config/environment.js
+++ config/environment.js
@@ -27,13 +27,10 @@ module.exports = function(environment) {
     },
   };
 
-  ENV['ember-simple-auth-token'] = {
-    serverTokenEndpoint: `${ENV.DS.host}/token`,
-    tokenPropertyName: 'access_token',
-  };
+
   ENV['ember-simple-auth'] = {
     baseURL: '/',
-    routeAfterAuthentication: 'guest'
+    routeAfterAuthentication: 'guest',
   };
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -55,8 +52,14 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.DS.host = 'https://kin-parenting.herokuapp.com';
 
   }
+
+  ENV['ember-simple-auth-token'] = {
+    serverTokenEndpoint: `${ENV.DS.host}/token`,
+    tokenPropertyName: 'access_token',
+  };
 
   return ENV;
 };
