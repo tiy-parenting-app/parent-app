c0128b9d82dcd590638dfe5529ee7d8986cf22e3
Alex T Leach <alextleach@gmail.com>
2016-08-30 14:17:19 -0500

    you can add the children

--- app/models/child.js
+++ app/models/child.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   age: DS.attr('number'),
-  careDetails: DS.attr('string'),
+  careDetails: DS.attr(),
 
   profile: DS.belongsTo('profile'),
 });
