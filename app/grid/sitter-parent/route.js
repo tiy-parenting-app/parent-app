ccab980e63d5c508332ecd940e6c76d7478f2b09
Hannah Swift <h.graceswift@gmail.com>
2016-08-29 20:50:01 -0500

    Data from API populates to parent cards on grid/parents page

--- app/grid/sitter-parent/route.js
+++ app/grid/sitter-parent/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model({ user }) {
+     return this.store.query('user', {
+      filter: {
+        account_type: 'parent',
+      },
+     })
+  },
 });
