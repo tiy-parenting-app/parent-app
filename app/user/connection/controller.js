a6a005d95ea7c9a14475229d43663a3e3bcc3750
Hannah Swift <h.graceswift@gmail.com>
2016-08-31 16:15:10 -0500

    Add five star ratings to connections page

---
+++ app/user/connection/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    selectRating(user, value) {
+      user.get('profile').then((profile) => {
+        const rating = this.store.createRecord('rating', {
+          profile, value,
+        })
+        rating.save();
+      });
+    },
+  },
+});
