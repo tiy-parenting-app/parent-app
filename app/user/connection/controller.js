ab4056d8cbe4da5a6b7a2d79a8d9933274797710
Hannah Swift <h.graceswift@gmail.com>
2016-09-01 17:48:05 -0500

    Sitter info and hourly rate show up in card and on profile

--- app/user/connection/controller.js
+++ app/user/connection/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+
     selectRating(user, value) {
       user.get('profile').then((profile) => {
         const rating = this.store.createRecord('rating', {
@@ -12,8 +13,20 @@ export default Ember.Controller.extend({
       });
     },
 
-    workPlease(profile) {
-      profile.toggleProperty('cow');
+    workPlease(user) {
+      user.get('profile').then((profile) => {
+        const myLike = profile.get('myLike');
+        if (myLike) {
+          myLike.toggleProperty('like');
+          return myLike.save();
+        }
+
+        const like = this.store.createRecord('like', {
+          profile,
+          like: true,
+        })
+        like.save();
+      });
     },
   },
 });
